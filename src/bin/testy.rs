1383d154efc864b80e17e7ac0d8a7e152cd368f7
J.T. Sage <jtsage+datebox@gmail.com>
2024-10-17 21:13:28 -0400

    test some multithreading

--- src/bin/testy.rs
+++ src/bin/testy.rs
@@ -4,37 +4,40 @@
 //! optionally including information on contained store
 //! items, along with savegame processing, and basic mod
 //! pack processing.
-use std::path::{self};
+use std::path::{self, PathBuf};
 use std::time::Instant;
 use glob::glob;
 use fs_mod_parser::parse_basic_mod;
+use rayon::prelude::*;
 
 fn main() {
     let start_time = Instant::now();
-    let mut counter: u64 = 0;
-    let mut last_duration;
 
-    let pattern = "./tests/test_mods/EXAMPLE_*";
-    // let pattern = "C:\\Users\\jtsag\\Documents\\My Games\\FarmingSimulator2022\\mods\\*\\*";
+    // let pattern = "./tests/test_mods/*";
+    let pattern = "C:\\Users\\jtsag\\Documents\\My Games\\FarmingSimulator2022\\mods\\*\\*";
     // let pattern = "C:\\Users\\jtsag\\Documents\\My Games\\FarmingSimulator2022\\mods\\maps_test\\*";
 
-    for entry in glob(pattern).unwrap().filter_map(Result::ok) {
-        last_duration = start_time.elapsed();
+    let file_list:Vec<PathBuf> = glob(pattern).unwrap().filter_map(Result::ok).collect();
+    let counter = file_list.len();
+
+    file_list.par_iter().for_each(|entry|{
+        let this_file_start = Instant::now();
         let file_metadata = std::fs::metadata(&entry).unwrap();
 
         match path::absolute(entry.clone()) {
             Ok(abs_path) => {
                 let _output = parse_basic_mod(abs_path.as_path(), file_metadata.is_dir());
-                counter += 1;
-                print!("{} in {:.2?}\n", entry.clone().to_str().unwrap(), start_time.elapsed() - last_duration);
-                print!("{}\n", _output);
+                // counter += 1;
+                print!("{} in {:.2?}\n", entry.clone().to_str().unwrap(), this_file_start.elapsed());
+                // print!("{}\n", _output);
             },
             Err(e) => panic!("{}", e),
         };
-    }
+    });
+
 
     let elapsed = start_time.elapsed();
-    println!("Elapsed: {:.2?} for {} files", elapsed, counter);
+    println!("Total Elapsed: {:.2?} for {} files", elapsed, counter);
 }
 
 
