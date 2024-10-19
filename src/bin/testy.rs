74f6b3e9389dd7f8400d86dae7665cb12e842d9a
J.T. Sage <jtsage+datebox@gmail.com>
2024-10-19 16:03:50 -0400

    refactor main test program (will eventually delete)

--- src/bin/testy.rs
+++ src/bin/testy.rs
@@ -4,20 +4,24 @@
 //! optionally including information on contained store
 //! items, along with savegame processing, and basic mod
 //! pack processing.
-use std::path::{self, PathBuf};
+use std::path::{self, PathBuf, Path};
 use std::time::Instant;
 use glob::glob;
-use fs_mod_parser::parse_basic_mod;
+use fs_mod_parser::{parse_basic_mod, savegame::parser};
 use rayon::prelude::*;
 
 fn main() {
+    let output = parser(Path::new("./tests/test_mods/VARIANT_SaveGame.zip"), false);
+    print!("{}", output.pretty_print());
+}
+
+#[allow(dead_code)]
+fn scan_test_items(pattern_part: &str) {
     let start_time = Instant::now();
 
-    // let pattern = "./tests/test_mods/*";
-    let pattern = "C:\\Users\\jtsag\\Documents\\My Games\\FarmingSimulator2022\\mods\\*\\*";
-    // let pattern = "C:\\Users\\jtsag\\Documents\\My Games\\FarmingSimulator2022\\mods\\maps_test\\*";
+    let pattern = format!("./tests/test_mods/{}", pattern_part);
 
-    let file_list:Vec<PathBuf> = glob(pattern).unwrap().filter_map(Result::ok).collect();
+    let file_list:Vec<PathBuf> = glob(pattern.as_str()).unwrap().filter_map(Result::ok).collect();
     let counter = file_list.len();
 
     file_list.par_iter().for_each(|entry|{
@@ -40,4 +44,32 @@ fn main() {
     println!("Total Elapsed: {:.2?} for {} files", elapsed, counter);
 }
 
+
+#[allow(dead_code)]
+fn scan_full_collection() {
+    let start_time = Instant::now();
+
+    let pattern = "C:\\Users\\jtsag\\Documents\\My Games\\FarmingSimulator2022\\mods\\*\\*";
+
+    let file_list:Vec<PathBuf> = glob(pattern).unwrap().filter_map(Result::ok).collect();
+    let counter = file_list.len();
+
+    file_list.par_iter().for_each(|entry|{
+        let this_file_start = Instant::now();
+        let file_metadata = std::fs::metadata(entry).unwrap();
+
+        match path::absolute(entry.clone()) {
+            Ok(abs_path) => {
+                let _output = parse_basic_mod(abs_path.as_path(), file_metadata.is_dir());
+
+                println!("{} in {:.2?}", entry.clone().to_str().unwrap(), this_file_start.elapsed());
+            },
+            Err(e) => panic!("{}", e),
+        };
+    });
+
+
+    let elapsed = start_time.elapsed();
+    println!("Total Elapsed: {:.2?} for {} files", elapsed, counter);
+}
 
